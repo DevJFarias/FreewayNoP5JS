0dcd841c1bf8db0d812711b217c5fa57d1d2108a
João Farias <jpafarias17@icloud.com>
2023-07-27 13:06:16 -0300

    Adicionando arquivos

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
